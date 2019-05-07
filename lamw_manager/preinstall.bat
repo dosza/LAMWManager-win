dee479de6ecf96302d38358809bc8faf5e77e59e
Daniel <oliveira.daniel109@gmail.com>
2019-05-07 16:27:20 -0300

    Fix powershell bug

--- lamw_manager/preinstall.bat
+++ lamw_manager/preinstall.bat
@@ -7,6 +7,6 @@ echo %PATH%
 echo %exec_path%
 systeminfo | findstr x64-based
 REM if %errorlevel% EQU 0 (preinstall-win64.bat) else (preinstall-win32.bat) 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass
+powershell.exe Set-ExecutionPolicy Bypass
 powershell newinstaller.ps1
 pause
