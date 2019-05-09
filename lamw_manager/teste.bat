9fc3589f52af6c776f086e281a6689a3407e4951
Daniel <oliveira.daniel109@gmail.com>
2019-05-09 18:24:22 -0300

    Add Getting Started

--- lamw_manager/teste.bat
+++
@@ -1,9 +0,0 @@
-
-@echo true
-SET ARCH="NONE"
-echo "%ARCH%"
-systeminfo | findstr "x64"
-if "%ERRORLEVEL%" EQU "0"  ( SET ARCH="x64" ) else (set ARCH="x86" )
-echo "%ARCH%"
-
-powershell $PSVersionTable.PSVersion
